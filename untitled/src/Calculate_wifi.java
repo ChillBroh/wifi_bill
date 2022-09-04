a5e1b5dbe5db7af0898059ae52335430e87149af
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-09-04 17:09:34 +0530

    created const variables using final

--- untitled/src/Calculate_wifi.java
+++ untitled/src/Calculate_wifi.java
@@ -1,2 +1,6 @@
 public class Calculate_wifi {
+    private final int aj = 30;
+    private final int ishara = 0;
+    private final int isuru = 35;
+    private final int sara = 35;
 }
---
+++ untitled/src/Wif_Main.java
@@ -0,0 +1,5 @@
+public class Wif_Main {
+    public static void main(String[] args) {
+
+    }
+}
